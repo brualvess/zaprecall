247d3d03820c6a53c0c6e8f330c657abae4a3dfc
brualvess <oliveirabrunna2210@gmail.com>
2022-05-14 22:28:06 -0300

    Pergunta do flashcard

--- src/Tela2.js
+++ src/Tela2.js
@@ -1,5 +1,47 @@
-export default function Tela2(){
-    return(
-        <h1>Olá, Maciel</h1>
-    )
+import React from 'react';
+import Topo from "./Topo"
+function Perguntas(props){
+    const[mostrarPergunta, setMostrarPergunta]=React.useState("valor inicial")
+
+return(
+    <>
+    {
+ (mostrarPergunta == "valor inicial") ? 
+        <div onClick={()=>setMostrarPergunta("hello")}className="perguntas"> 
+        <h2>Pergunta {props.numero}</h2>
+        <ion-icon  class="play" name="play-outline"></ion-icon>
+        </div>
+    : 
+    <div className="pergunta">
+        <div className="renderPergunta">
+            {props.questao}
+        </div>
+        <div  class="iconePergunta"><ion-icon class="ion" name="arrow-redo-circle"></ion-icon></div>
+    </div>
+    }
+  </>
+)
+
+}
+export default function Tela2() {
+    
+    const perguntas = [
+        {
+            pergunta : "O que é JSX?",
+            resposta : " Uma extensão de linguagem do JavaScript"
+        },
+        {
+            pergunta : " O React é __ ",
+            resposta : " uma biblioteca JavaScript para construção de interfaces"
+        },
+
+    ]
+    return (
+        <>
+        <Topo />
+        <div className="containerDePerguntas">
+        {perguntas.map((pergunta, index)=> <Perguntas numero = {index + 1} questao = {pergunta.pergunta}/>)}
+        </div>
+        </>
+        )
 }
---
+++ src/Topo.js
@@ -0,0 +1,12 @@
+
+
+export default function Topo(){
+    return(
+        <>
+        <div className="topo">
+            <img className="logo"src="imagens/logo-pequeno.png"/>
+            <h1>ZapRecall</h1>
+        </div>
+        </>
+    )
+}
